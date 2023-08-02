97847da9cb420671a35a5290d39a76a6cd5a284f
Darren Reid <darren@reidmail.org>
2023-08-02 12:02:33 +1000

    Initial commit

---
+++ DockerApp1.ServiceInterface/MyServices.cs
@@ -0,0 +1,12 @@
+using ServiceStack;
+using DockerApp1.ServiceModel;
+
+namespace DockerApp1.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+    }
+}
---
+++ DockerApp1/Configure.AppHost.cs
@@ -0,0 +1,25 @@
+using Funq;
+using ServiceStack;
+using DockerApp1.ServiceInterface;
+
+[assembly: HostingStartup(typeof(DockerApp1.AppHost))]
+
+namespace DockerApp1;
+
+public class AppHost : AppHostBase, IHostingStartup
+{
+    public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureServices(services => {
+            // Configure ASP.NET Core IOC Dependencies
+        });
+
+    public AppHost() : base("DockerApp1", typeof(MyServices).Assembly) {}
+
+    public override void Configure(Container container)
+    {
+        // Configure ServiceStack only IOC, Config & Plugins
+        SetConfig(new HostConfig {
+            UseSameSiteCookies = true,
+        });
+    }
+}
